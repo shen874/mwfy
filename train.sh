1a858a318871cbc545a6f1cdb12dd78175b86974
shen874 <sls_193199283@163.com>
2026-01-11 14:59:50 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-DATA_DIR=${1:-"./data"}
-python -u src/train_braille.py --data_dir "$DATA_DIR" --epochs 15 --batch_size 128 --d_model 384 --nhead 6 --num_layers 4 --ffn_dim 1024 --dropout 0.1 --lr 3e-4 --max_len 1024
